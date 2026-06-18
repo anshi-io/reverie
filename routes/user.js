729623db1bebd980f2ce977b7cb551c5f2b4da3c
Anshi Malik <anshimalik64@gmail.com>
2026-06-18 12:27:31 +0530

    updated listing page styling

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/user.js");
 router.get("/", (req,res)=>{
-    res.render("/listings");
+    res.redirect("/listings");
 });
 router.route("/signup")
 .get(userController.renderSignup)
